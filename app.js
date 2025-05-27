fb031638817e574084b9101b9ab31b669587becf
Surabhi M R <surabhimr27@gmail.com>
2025-05-27 23:22:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// const add=require("./math");
+// const mult=require("./math");
+
+const math=require("./math");
+
+console.log(math.add(5,10));
+console.log(math.mul(5,10));
+console.log(math.sub(5,10));
+console.log(math.div(5,10));
+// common JS
