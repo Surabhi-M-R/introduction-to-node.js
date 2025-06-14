e8fd5b8df6d4713d0354991b7d21fec54306ea24
Surabhi M R <surabhimr27@gmail.com>
2025-06-14 10:45:43 +0530

    commit ECMA script

---
+++ ESmodule/app.mjs
@@ -0,0 +1,12 @@
+// const add=require("./math");
+// const mult=require("./math");
+
+//const math=require("./math");
+import mul from "./math.mjs";
+
+//console.log(math.add(5,10));
+console.log(mul(5,10));
+//console.log(math.sub(5,10));
+//console.log(math.div(5,10));
+// common JS
+
